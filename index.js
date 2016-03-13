545b404565b1142105fae6dc0c6ec4523adbc054
Ji Baik <Ji@Jis-MacBook-Pro.local>
2016-03-12 16:38:59 -0800

    add twilio send

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var config = require("./config.js");
 var express = require("express");
 var app = express();
+var client = require('twilio')(config.TWILIO.ACCOUNT_SID , config.TWILIO.AUTH_TOKEN);
 
-console.log("Hello World");
+// console.log("account SID ="+config.TWILIO.AUTH_TOKEN);
+
+client.sendMessage({
+	to: '+14084833869',
+	from:'+13049072742',
+	body:'yo you sexy mofo'
+	
+}, function(err, responseData){
+	if(!err){
+
+		console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+	}
+
+});
 
 app.get('/', function(req, res){
     res.send('you only yolo once');
